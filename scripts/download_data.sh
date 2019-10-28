e96a77abc6570f6af1a3a5e4d0763c031ed302a5
Beres Ferenc <ferdzso05@gmail.com>
2019-10-28 12:55:20 +0100

    data download script were added

---
+++ scripts/download_data.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://dms.sztaki.hu/~fberes/ln/ln_data.zip
+unzip ln_data.zip
--- scripts/run_simulator.py
+++ scripts/run_simulator.py
@@ -3,7 +3,7 @@ import os, sys
 sys.path.insert(0,"../python/")
 import simulator.transaction_simulator as ts
 
-# 1. Load parameters
+print("# 1. Parameters")
 
 experiment_id = "trial"
 snapshot_id = 0
@@ -15,30 +15,34 @@ drop_low_cap = True
 with_depletion = True
 find_alternative_paths = True
 
+data_dir = "../ln_data/"
 output_dir = "%s/%i/" % (experiment_id, snapshot_id)
 
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-# 2. Load data
+print("# 2. Load data")
 
-snapshots = pd.read_csv("/mnt/idms/fberes/data/LNdata/ln_snapshot_directed_multi_edges.csv")
-node_meta = pd.read_csv("/mnt/idms/fberes/data/LNdata/1ml_meta_data.csv")
+snapshots = pd.read_csv("%s/ln_snapshot_directed_multi_edges.csv" % data_dir)
+node_meta = pd.read_csv("%s/1ml_meta_data.csv" % data_dir)
+print("Data files were LOADED.")
+
 providers = list(node_meta["pub_key"])
 edges = snapshots[snapshots["snapshot_id"]==snapshot_id]
 
-# 3. Simulation
+print("# 3. Simulation")
 
-simulator = ts.TransactionSimulator(edges, providers, amount_sat, num_transactions, drop_disabled=drop_disabled, drop_low_cap=drop_low_cap, eps=eps, with_depletion=with_depletion)
+simulator = ts.TransactionSimulator(edges, providers, amount_sat, num/mnt/idms/fberes/data/LNdata_transactions, drop_disabled=drop_disabled, drop_low_cap=drop_low_cap, eps=eps, with_depletion=with_depletion)
 transactions = simulator.transactions
 shortest_paths, alternative_paths, all_router_fees, _ = simulator.simulate(weight="total_fee", with_node_removals=find_alternative_paths)
 total_income, total_fee = simulator.export(output_dir)
 
-# 4. Stats
+print("# 4. Statistics")
 
 print("Total income:", total_income.sum())
 
-# 5. Analyze optimal routing fee for nodes
+print("# 5. Analyze optimal routing fee for nodes")
+
 if find_alternative_paths:
     opt_fees_df, p_altered = ts.calc_optimal_base_fee(shortest_paths, alternative_paths, all_router_fees)
     opt_fees_df.to_csv("%s/opt_fees.csv" % output_dir, index=False)
